60e7c35fd7074fe66ee4dd33948b3e46ed228bdc
narine <narinemkn@gmail.com>
2020-07-07 02:48:27 +0200

    Updates

---
+++ precache-manifest.714c9f4a21c87839b373d7de6e4d4148.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f0861aaa4d77bbfe8cba34d83babf81",
+    "url": "/19Nar/RestaurantAPI-React/index.html"
+  },
+  {
+    "revision": "ebed8044d5a842947682",
+    "url": "/19Nar/RestaurantAPI-React/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "4dbfeb46f056977e3db9",
+    "url": "/19Nar/RestaurantAPI-React/static/css/main.0b190c47.chunk.css"
+  },
+  {
+    "revision": "ebed8044d5a842947682",
+    "url": "/19Nar/RestaurantAPI-React/static/js/2.66d107de.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/19Nar/RestaurantAPI-React/static/js/2.66d107de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4dbfeb46f056977e3db9",
+    "url": "/19Nar/RestaurantAPI-React/static/js/main.2c1e3790.chunk.js"
+  },
+  {
+    "revision": "c982ca5b0ed0138df951",
+    "url": "/19Nar/RestaurantAPI-React/static/js/runtime-main.c9026d16.js"
+  }
+]);
